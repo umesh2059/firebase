e85cff9f2ce69009647ba8f6d03eb32443be8abf
umesh gupta <umeshg2059@gmail.com>
2025-09-29 09:28:24 +0530

    authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
-import {getDatabase,ref,set} from "firebase/database"
+// import {getDatabase,ref,set} from "firebase/database"
+import {getAuth,createUserWithEmailAndPassword} from "firebase/auth";
 import {app} from "./firebase"
 import './App.css'
-const db=getDatabase(app);
+// const db=getDatabase(app);
+
+
+const Auth=getAuth(app)
 
 
 function App() {
-  const putData=()=>{
-    set(ref(db,"users/umesh"),{
-      id:1,
-      name:"umesh gupta",
-      age:23,
-    })
-    .then(()=>{
-      console.log("data saved successfully");
-    })
-    .catch(()=>{
-      console.log("error saving data:",error)
-    })
 
-  };
+  const signupUser=()=>{
+    createUserWithEmailAndPassword(Auth,
+      "umeshg2059@gmail.com",
+      "umehskfdj123"
+    )
+    .then((value)=>console.log(value))
+
+  }
+  // const putData=()=>{
+  //   set(ref(db,"users/umesh"),{
+  //     id:1,
+  //     name:"umesh gupta",
+  //     age:23,
+  //   })
+  //   .then(()=>{
+  //     console.log("data saved successfully");
+  //   })
+  //   .catch(()=>{
+  //     console.log("error saving data:",error)
+  //   })
+
+  // };
   return (
    <div className='App'>
     <h1>hello how are you </h1>
-    <button  onClick={putData}>put data</button>
+    <button  onClick={signupUser}>create data</button>
    </div>
       
   )
 }
-export default App
+export default App;
