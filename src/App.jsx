27eac00537e2b564141848ab104fff4d22f56044
umesh gupta <umeshg2059@gmail.com>
2025-10-04 09:29:16 +0530

    authentication adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,44 @@
 // import {getDatabase,ref,set} from "firebase/database"
-import {getAuth,createUserWithEmailAndPassword} from "firebase/auth";
+import { useEffect,useState} from "react";
+import {getAuth,onAuthStateChanged,signOut} from "firebase/auth";
 import {app} from "./firebase"
 import SignupPage from './pages/signup'
 import SigninPage from './pages/signin'
 
 import './App.css'
 // const db=getDatabase(app);
-
-
-
 const Auth=getAuth(app);
-
-
 function App() {
 
-  const SignupUser=()=>{
-    createUserWithEmailAndPassword(Auth,"umeshsdjf2059@gmail.com","umehskfdj123"
-    )
-    .then((value)=>console.log(value))
-    .catch((error) => console.log(error));
-  };
-  // const putData=()=>{
-  //   set(ref(db,"users/umesh"),{
-  //     id:1,
-  //     name:"umesh gupta",
-  //     age:23,
-  //   })
-  //   .then(()=>{
-  //     console.log("data saved successfully");
-  //   })
-  //   .catch(()=>{
-  //     console.log("error saving data:",error)
-  //   })
+  const [user,setUser]=useState(null);
+  useEffect(()=>{
+    onAuthStateChanged(Auth,user=>{
+      if(user){
+        setUser(user);
+        // yes you are logged in 
+      
+      }else{
+        // user is logged out 
+        console.log("you are logged out ");
+        setUser(null);
+      }
+    });
+  },[])
 
-  // };
-  return (
+  if (user===null){
+
+     return (
    <div className='App'>
     <SignupPage/>
     <SigninPage/>
+   </div>
+     )
+  }
+
+  return (
+   <div className='App'>
+  <h1>Hello {user.email}</h1>
+  <button onclick={()=>signOut(Auth)}>logout</button>
    </div>
       
   );
